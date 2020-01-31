12a5923ff5149df65bea9a2e411dcbffcb2063ff
CaiYueTing <qazxcv367tasi@gmail.com>
2020-01-31 17:12:28 +0800

    feat: simple websocket

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"gopkg.in/olahol/melody.v1"
+)
+
+func main() {
+	r := gin.Default()
+	m := melody.New()
+	r.GET("/", func(c *gin.Context) {
+		http.ServeFile(c.Writer, c.Request, "templates/index.html")
+	})
+
+	r.GET("/ws", func(c *gin.Context) {
+		m.HandleRequest(c.Writer, c.Request)
+	})
+	m.HandleMessage(func(s *melody.Session, msg []byte) {
+		m.Broadcast(msg)
+		servermsg := "<Sever> suprise mother fucker"
+		m.Broadcast([]byte(servermsg))
+	})
+	r.Run(":8080")
+}
